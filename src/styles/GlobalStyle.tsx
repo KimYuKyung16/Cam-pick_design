0b232979998ad00846a714295d1d62e63621980d
kyk <kyg0954@naver.com>
2023-02-19 20:43:23 +0900

    initialize

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,17 @@
+/** 
+ * 전역 스타일
+ * 
+ * */ 
+
+ import { createGlobalStyle } from "styled-components";
+ 
+ const GlobalStyle = createGlobalStyle`
+   *, *::before, *::after {
+     box-sizing: border-box;
+   }
+   body {
+    
+   }
+ `;
+ 
+ export default GlobalStyle;
