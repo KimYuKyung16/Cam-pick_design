424fd6e25dfffbe0eb1f23709521a081e31125e1
kyk <kyg0954@naver.com>
2023-02-22 23:08:44 +0900

    style: default

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -8,7 +8,10 @@
  const GlobalStyle = createGlobalStyle`
    *, *::before, *::after {
      box-sizing: border-box;
+     padding: 0;
+     margin: 0;
    }
+   
    body {
     
    }
---
+++ src/styles/color.tsx
@@ -0,0 +1,15 @@
+/**
+ * 
+ * 색감 정보
+ */
+
+const colorConfig = {
+  primary: "#FE6834",
+  secondary: "#F2D041",
+  white: "#FDFBF8",
+  basic: "#2C2C2C",
+  pri_sub: "#FFEFE9",
+  gary: "#7A7A7A",
+}
+
+export default colorConfig;
